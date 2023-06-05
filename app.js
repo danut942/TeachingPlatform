97412dac3616fdd90964feb4bea89f803b6aa8eb
Danut Sacu <danut.sacu@gmail.com>
2023-06-05 17:21:39 +0300

    add additional modules

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const bodyParser = require("body-parser")
 const ejs = require("ejs");
+const mongoose = req
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+mongoose.connect("mongodb://127.0.0.1:27017/teachingDB");
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
 
 app.get('/',(req,res) => {
     res.send('Teaching Home Page');
