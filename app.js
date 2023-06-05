22db9f8523e3c8a36efcccc48505f17eaf751b94
Danut Sacu <danut.sacu@gmail.com>
2023-06-05 16:03:12 +0300

    Create project structure and work on login page

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const ejs = require("ejs");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+
 
 app.get('/',(req,res) => {
-    res.send('Hello World')
+    res.send('Hello World');
 })
 
-
-
-
-
+app.get('/login', (req,res) => {
+    res.render("login");
+})
 
 app.listen(port,() => {
     console.log('App listening on port',port)
