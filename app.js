d36d048337fef9194227504478fac5221937d872
Danut Sacu <danut.sacu@gmail.com>
2023-06-13 00:25:19 +0300

    add views for contact and other tabs

--- app.js
+++ app.js
@@ -20,10 +20,16 @@ const userSchema = new mongoose.Schema({
     password:String
 });
 
+const newsSchema = new mongoose.Schema({
+    message:String
+});
+
+
 const secret = "Thisisthesecret";
 userSchema.plugin(encrypt,{secret: secret, encryptedFields:["password"]});
 
 const User = new mongoose.model("User",userSchema);
+const Anunt = new mongoose.model("Anunt",newsSchema);
 
 app.get('/',(req,res) => {
     res.render("teaching");
@@ -41,6 +47,18 @@ app.get('/home', (req,res) => {
     res.render("home");
 })
 
+app.get('/noutati', (req,res) => {
+    res.render("noutati");
+})
+
+app.get('/cursuri', (req,res) => {
+    res.render("cursuri");
+})
+
+app.get('/contact', (req,res) => {
+    res.render("contact");
+})
+
 // register an user and render teaching home page if successfull
 app.post("/register",(req,res) => {
     const newUser = new User({
@@ -51,6 +69,7 @@ app.post("/register",(req,res) => {
     newUser.save();
     res.redirect("home");
 });
+
 
 // check for an existing user and login
 app.post("/login",(req,res) => {
@@ -65,6 +84,27 @@ app.post("/login",(req,res) => {
         }
     }
     runFindUser().catch(console.dir);
+});
+
+// register an user and render teaching home page if successfull
+app.post("/noutati",(req,res) => {
+    const newAnunt = new Anunt({
+        message: req.body.message,
+    })
+
+    newAnunt.save();
+    res.redirect("noutati");
+});
+
+// get all news feeds
+app.get("/noutati",(req,res) => {
+
+    async function runFindMessages(){
+        const foundMessage = await User.find();
+
+        res.render("/noutati",{value: foundMessage});
+    }
+    runFindMessages().catch(console.dir);
 });
 
 
